937f3c03eecbd6a9117de33514389b74be91cc62
Abbas Angouti <g.a.angouti@gmail.com>
2018-08-31 09:54:39 -0700

    added constants file

---
+++ MovieSearchDemo/Constant/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  MovieSearchDemo
+//
+//  Created by Abbas Angouti on 8/30/18.
+//  Copyright © 2018 Abbas Angouti. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct URLs {
+        static let apiBaseURL = "https://api.themoviedb.org/3/search/movie" // to get metadata
+        static let imageBaseURL = "https://image.tmdb.org/t/p/w600_and_h900_bestv2" // to download posters
+    }
+    
+    
+    struct Keys {
+        static let APIKey = "2a61185ef6a27f400fd92820ad9e8537"
+    }
+}
+
